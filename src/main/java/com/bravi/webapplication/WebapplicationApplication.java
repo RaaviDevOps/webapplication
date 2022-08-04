19de703643a4982512efed26114160792251f7bc
Brahmaiah Ravi <brammaiah@gmail.com>
2022-08-04 14:49:53 -0400

    Added initial project

---
+++ src/main/java/com/bravi/webapplication/WebapplicationApplication.java
@@ -0,0 +1,14 @@
+package com.bravi.webapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebapplicationApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bravi/webapplication/WebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravi.webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
